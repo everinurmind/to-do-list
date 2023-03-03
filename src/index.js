8bec4b0b63c6eca7f117f039b676b228e36576de
everinurmind <nurbol.sultanov@gmail.com>
2023-03-03 17:04:06 -0600

    made requested changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+// Import styles
 import './style.css';
-import {
-  form, addTask, renderList, clearCompletedTasks,
-} from './modules/utilities.js';
 
+// Import variables and functions from modules
+import { form, newTaskBtn, clearBtn } from './modules/variables.js';
+import { addTask, renderList } from './modules/functions.js';
+import { clearCompletedTasks } from './modules/methods.js';
+
+// Render the list on page load
 renderList();
-clearCompletedTasks();
+
+// Add event listener to the form for adding new task
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addTask();
 });
+
+// Add event listener to the new task button for adding new task
+newTaskBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addTask();
+});
+
+// Add event listener to the clear button for clearing completed tasks
+clearBtn.addEventListener('click', clearCompletedTasks);
