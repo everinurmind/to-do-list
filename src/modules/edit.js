0794c52185aac902d486e40eaee264a61be372b2
everinurmind <nurbol.sultanov@gmail.com>
2023-03-10 16:23:01 -0600

    add edit/update/clear setup

---
+++ src/modules/edit.js
@@ -0,0 +1,16 @@
+export const updateStatus = (id) => {
+    const toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
+    const checkbox = document.querySelector(`.checkbox[data-id="${id}"]`);
+    const index = toDoList.findIndex((task) => task.index === +id);
+    if (checkbox) {
+      toDoList[index].completed = checkbox.checked;
+      localStorage.setItem('toDoList', JSON.stringify(toDoList));
+    }
+  };
+  
+  export const clearCompletedTasks = () => {
+    let toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
+    toDoList = toDoList.filter((task) => !task.completed);
+    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+  };
+  
