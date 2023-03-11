43f4a9b604d7affaa082774d70ffef5f4a16c22a
everinurmind <nurbol.sultanov@gmail.com>
2023-03-10 18:38:17 -0600

    add editTask, clearCompletedTasks function test

--- src/modules/edit.js
+++ src/modules/edit.js
@@ -1,11 +1,25 @@
-export const updateStatus = (id) => {
-  const toDoList = JSON.parse(localStorage.getItem('toDoList')) || [];
-  const checkbox = document.querySelector(`.checkbox[data-id="${id}"]`);
-  const index = toDoList.findIndex((task) => task.index === +id);
-  if (checkbox) {
-    toDoList[index].completed = checkbox.checked;
-    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+export const editTask = (id) => {
+  const options = document.querySelector(`.las.la-ellipsis-v[data-id="${id}"]`);
+  const remove = document.querySelector(`.las.la-trash-alt[data-id="${id}"]`);
+  const description = document.querySelector(`.task-description[data-id="${id}"]`);
+  const task = document.querySelector(`li[data-id="${id}"]`);
+
+  task.classList.add('editing');
+  options.classList.add('hide');
+  remove.classList.add('active');
+  description.readOnly = false;
+};
+
+export const updateTask = (id, toDoList) => {
+  const editedDescription = document.querySelector(`.task-description[data-id="${id}"]`);
+  if (editedDescription.value.trim() === '') {
+    return;
   }
+
+  const index = toDoList.findIndex((task) => task.index === +id);
+
+  toDoList[index].description = editedDescription.value;
+  localStorage.setItem('toDoList', JSON.stringify(toDoList));
 };
 
 export const clearCompletedTasks = () => {
@@ -13,4 +27,3 @@ export const clearCompletedTasks = () => {
   toDoList = toDoList.filter((task) => !task.completed);
   localStorage.setItem('toDoList', JSON.stringify(toDoList));
 };
-  
--- src/modules/edit.test.js
+++ src/modules/edit.test.js
@@ -1,16 +1,63 @@
-import { updateStatus } from "./edit";
+import { editTask, updateTask, clearCompletedTasks } from './edit.js';
 
-describe('Update status', () => {
-    test('Update task status', () => {
-        const input = document.createElement('input');
-        input.type = 'checkbox';
-        input.id = 'new-task';
-        input.checked = true;
-        const list = document.createElement('ul');
-        list.appendChild(input);
-        document.body.appendChild(list);
-        const localObject = JSON.parse(localStorage.getItem('toDoList'));
-        updateStatus('new-task');
-        expect(localObject[0].completed).toBe(false);
-    })
-})
+document.body.innerHTML = `
+  <ul>
+    <li data-id="123">
+      <span class="las la-ellipsis-v" data-id="123"></span>
+      <input type="text" class="task-description" data-id="123" value="Buy groceries" readonly>
+      <span class="las la-trash-alt" data-id="123"></span>
+    </li>
+  </ul>
+`;
+
+describe('editTask function', () => {
+  test('editTask function should set the task as "editing"', () => {
+    editTask('123');
+
+    const options = document.querySelector('.las.la-ellipsis-v[data-id="123"]');
+    const remove = document.querySelector('.las.la-trash-alt[data-id="123"]');
+    const description = document.querySelector('.task-description[data-id="123"]');
+    const task = document.querySelector('li[data-id="123"]');
+
+    expect(task.classList.contains('editing')).toBe(true);
+    expect(options.classList.contains('hide')).toBe(true);
+    expect(remove.classList.contains('active')).toBe(true);
+    expect(description.readOnly).toBe(false);
+  });
+});
+
+const toDoList = [
+  {
+    index: 123,
+    description: 'Buy groceries',
+    completed: false,
+  },
+  {
+    index: 456,
+    description: 'Do laundry',
+    completed: true,
+  },
+];
+localStorage.setItem('toDoList', JSON.stringify(toDoList));
+
+describe('updateTask function', () => {
+  test('updateTask function should update the task description in the toDoList and render the updated list', () => {
+    updateTask('123', toDoList);
+    const updatedToDoList = JSON.parse(localStorage.getItem('toDoList'));
+    expect(updatedToDoList[0].description).toBe('Buy groceries');
+    expect(updatedToDoList.length).toBe(2);
+  });
+});
+
+describe('clearCompletedTasks function', () => {
+  test('clearCompletedTasks should remove completed tasks from the toDoList array and update local storage', () => {
+    const completedTask1 = { id: 1, description: 'Task 1', completed: true };
+    const completedTask2 = { id: 2, description: 'Task 2', completed: true };
+    const incompleteTask = { id: 3, description: 'Task 3', completed: false };
+    const toDoList = [completedTask1, completedTask2, incompleteTask];
+    localStorage.setItem('toDoList', JSON.stringify(toDoList));
+    clearCompletedTasks();
+    const updatedToDoList = JSON.parse(localStorage.getItem('toDoList'));
+    expect(updatedToDoList).toEqual([incompleteTask]);
+  });
+});
